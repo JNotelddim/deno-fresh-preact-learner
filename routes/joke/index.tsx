1028cea07d09ca0fa36f9d91438009f860a980f7
Jared McKimm <jaredmiddleton3.14@gmail.com>
2024-07-11 15:57:30 -0700

    Try using preact signals instead of the state hooks; no error but no result

--- routes/joke/index.tsx
+++ routes/joke/index.tsx
@@ -1,21 +1,28 @@
-import { useEffect, useState } from "preact/hooks"; // "https://esm.sh/v128/preact@10.19.6/hooks/src/index.d.ts";
+// import { useEffect, useState } from "preact/hooks"; // "https://esm.sh/v128/preact@10.19.6/hooks/src/index.d.ts";
+import { computed, effect, signal } from "@preact/signals";
 
 export default function JokePage() {
     // State to store the fetched joke
-    const [joke, setJoke] = useState("");
+    const joke = signal<string | undefined>(undefined);
 
     // Fetch a joke when the component mounts
-    useEffect(() => {
+    effect(() => {
+        console.log("fetching joke");
         fetch("/api/jokes")
             .then((response) => response.text())
-            .then((text) => setJoke(text))
+            .then((text) => {
+                joke.value = text;
+                console.log("Fetched joke:", text);
+            })
             .catch((error) => console.error("Failed to fetch joke:", error));
-    }, []); // Empty dependency array means this effect runs once on mount
+    });
+
+    const prettifiedJoke = computed(() => joke || "Loading joke...");
 
     return (
         <div>
             <h1>Welcome to the Joke Page!</h1>
-            <p>{joke || "Loading joke..."}</p>
+            <p>{prettifiedJoke}</p>
         </div>
     );
 }
